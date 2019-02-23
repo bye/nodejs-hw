677c8981bdec64020792f88aac013027789fe58c
Irina Chushkina <irina.chushkina@iii.com>
2019-02-24 00:18:46 +0300

    Small changes in structure

--- src2/app.js
+++ src2/app.js
@@ -1,9 +1,9 @@
 export const express = require('express');
 const cookieParser = require('cookie-parser');
-import router from './routes';
+import apiRouter from './routes/api';
 export const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
 
-app.use('/', router)
+app.use('/', apiRouter)
