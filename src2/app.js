b09f9313ae1e65459df8e4b1cf4dc63385d09822
Irina Chushkina <irina.chushkina@iii.com>
2019-03-06 00:29:31 +0300

    Added authRoutes with passport-google strategy

--- src2/app.js
+++ src2/app.js
@@ -1,9 +1,11 @@
 export const express = require('express');
 const cookieParser = require('cookie-parser');
 import apiRouter from './routes/api';
+import authRouter from './routes/auth'
 export const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
 
-app.use('/', apiRouter)
+app.use('/', apiRouter);
+app.use('/auth', authRouter);
